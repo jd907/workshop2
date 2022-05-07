9f3bb516761cd290dd31f6412852e7b05c8f10ea
jd907 <jademientieff@gmail.com>
2022-05-07 11:12:53 -0800

    fixed urls

--- app/nc_tutorials/urls.py
+++ app/nc_tutorials/urls.py
@@ -22,4 +22,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('tutorials.urls')),
     path('', include('users.urls'))
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
